c220a41cc9258cfe0ee6ecafcf6ba0249a68565a
BiankaPerez <a330100@uach.mx>
2023-05-06 18:10:27 -0700

    Add variables.py file

---
+++ variables.py
@@ -0,0 +1,14 @@
+name = "Bianka" 
+last_name = "Perez"
+age = 23
+list_shopping = ["apples", "peaches", "soap"]
+
+print(f"{name}, {age}")
+
+print(name + " " + last_name + " " + str(age))
+
+print(list_shopping)
+
+  
+
+
