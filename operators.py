1ea1e001b7931dbcaf2ed6262adb83463eea63a4
BiankaPerez <a330100@uach.mx>
2023-05-08 16:17:26 -0700

    ADD: operators

---
+++ operators.py
@@ -0,0 +1,26 @@
+adittion = 5+ 3 # esto da como resultado 8
+substraction = 4-2 # esto dara como resultado 2
+multiplication = 3*2 # esto dara como resultado 6
+
+#Comparasion operators 
+a = 10
+b = 20
+
+(a == b)  # Output: False
+(a != b)  # Output: True
+(a < b)   # Output: True
+(a > b)   # Output: False
+(a <= b)  # Output: True
+(a >= b)  # Output: False
+
+#Logical Operators 
+age_boy =10
+age_girl =14
+
+age_boy >8 and age_girl >8 #true
+
+#Assigment operators
+age= 10 # Dara 10
+age += 5 # Dara 15
+age -= 5 # Dara 10
+age *= 5 # Dara 50
